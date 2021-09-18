084389c29f8b391b486989d41bbb02d5c225713a
EUNHEE JANG <6810779s@naver.com>
2021-09-18 11:36:08 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MovieList from './components/MovieList';
-import MainImage from './components/MainImage';
 import './App.css';
 
 const App = () => {
   return (
     <div>
-      <MainImage />
       <MovieList />
     </div>
   );
